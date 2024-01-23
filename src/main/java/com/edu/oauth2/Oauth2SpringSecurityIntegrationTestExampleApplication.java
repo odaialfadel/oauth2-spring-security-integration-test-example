b15339a978e1e478e6c81e9bf41485d76540d48e
Odai Al Fadel <odai.deu@gmail.com>
2024-01-23 19:46:18 +0100

    feat(Oauth2SpringSecurityIntegrationTestExample) initial

---
+++ src/main/java/com/edu/oauth2/Oauth2SpringSecurityIntegrationTestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.edu.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SpringSecurityIntegrationTestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2SpringSecurityIntegrationTestExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/oauth2/Oauth2SpringSecurityIntegrationTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SpringSecurityIntegrationTestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
