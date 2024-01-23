141e7d5f7818016f1b6681628c39a202ac894736
Odai Al Fadel <odai.deu@gmail.com>
2024-01-23 20:45:44 +0100

    feat(Oauth2SpringSecurityIntegrationTestExample) created test config class for the integration test

--- src/test/java/com/edu/oauth2/Oauth2SpringSecurityIntegrationTestExampleApplicationTests.java
+++ src/test/java/com/edu/oauth2/Oauth2SpringSecurityIntegrationTestExampleApplicationTests.java
@@ -2,12 +2,11 @@ package com.edu.oauth2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integrationtest")
+@Import(Oauth2SpringSecurityIntegrationTestExampleApplicationTests.class)
 class Oauth2SpringSecurityIntegrationTestExampleApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
